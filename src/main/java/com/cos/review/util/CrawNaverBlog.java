4439081db18675c41b7918cc3b686933e2c394a9
baegopa <baegopa@wamwam.com>
2020-08-28 14:06:29 +0900

    200828 reviewApp - 3. 크롤링 데이터 날짜 파싱 DB 데이터 넣기 완료

--- src/main/java/com/cos/review/util/CrawNaverBlog.java
+++ src/main/java/com/cos/review/util/CrawNaverBlog.java
@@ -11,49 +11,11 @@ import com.cos.review.model.Product;
 
 public class CrawNaverBlog {
 
+	// 스프링 스케줄 라이브러리 써서 스케줄링 할 것!
 	public List<Product> startDayCraw(String keyword) {
-		int start = 1; // 10씩 증가하면 됨.
-		List<Product> products = new ArrayList<>();
-		while (products.size() < 1001) {
-			String url = "https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&query="
-					+ keyword + "&sm=tab_pge&srchby=all&st=sim&where=post&start=" + start;
 
-			try {
-				Document doc = Jsoup.connect(url).get();
-				Elements els1 = doc.select(".blog .sh_blog_top .sh_blog_title");
-				Elements els2 = doc.select(".blog .sh_blog_top .txt_inline");
-				Elements els3 = doc.select(".blog .sh_blog_top .sp_thmb img");
-				for (int i = 0; i < els1.size(); i++) {
-					Product product = new Product();
-					product.setTitle(els1.get(i).attr("title"));
-					product.setBlogUrl(els1.get(i).attr("href"));
-					product.setDay(els2.get(i).text());
-					if (els3.size() <= i) {
-						product.setThumnail("사진없음");
-					} else {
-						product.setThumnail(els3.get(i).attr("src"));
-					}
-					products.add(product);
-				}
-				System.out.println("start : " + start);
-				start = start + 10;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		for (Product product : products) {
-			System.out.println("===========================");
-			System.out.println("제목 : " + product.getTitle());
-			System.out.println("주소 : " + product.getBlogUrl());
-			System.out.println("섬네일 : " + product.getThumnail());
-			System.out.println("날짜 : " + product.getDay());
-			System.out.println();
-		}
-
-		return products;
+		return null;
 	}
-	
 
 	public List<Product> startAllCraw(String keyword) {
 
@@ -73,7 +35,7 @@ public class CrawNaverBlog {
 					product.setTitle(els1.get(i).attr("title"));
 					product.setBlogUrl(els1.get(i).attr("href"));
 					product.setDay(els2.get(i).text()); // .contain("전") 추가
- 					if (els3.size() <= i) {
+					if (els3.size() <= i) {
 						product.setThumnail("사진없음");
 					} else {
 						product.setThumnail(els3.get(i).attr("src"));
