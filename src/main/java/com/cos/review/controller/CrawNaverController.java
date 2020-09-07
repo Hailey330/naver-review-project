7fae2c9b6ea6768b45de19eff951bb23398ad6bc
baegopa <baegopa@wamwam.com>
2020-09-07 17:47:14 +0900

    200907 reviewApp - 5. 크롤링 자동 스케줄링 구현 완료, 관리자 목록 뿌리기 구현중

--- src/main/java/com/cos/review/controller/CrawNaverController.java
+++ src/main/java/com/cos/review/controller/CrawNaverController.java
@@ -1,7 +1,12 @@
 package com.cos.review.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +29,13 @@ public class CrawNaverController {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private SearchKeywordRepository searchKeywordRepository;
-	
+
 	@GetMapping("/product")
 	public @ResponseBody List<Product> product() {
-		return null;
+		return productRepository.findAll();
 	}
 
 	@GetMapping("/product/{keyword}")
@@ -38,15 +43,13 @@ public class CrawNaverController {
 		System.out.println("keyword : " + keyword);
 		return null;
 	}
-	
-	
-	
-//	@GetMapping("/searchKeyword")
-//	public @ResponseBody List<SearchKeyword> searchKeyword() {
-//		System.out.println("searchkeyword 호출됨");
-//		return searchKeywordRepository.findAll();
-//	}
-	
+
+	@GetMapping("/searchKeyword")
+	public @ResponseBody List<SearchKeyword> searchKeyword() {
+		System.out.println("searchkeyword 호출됨");
+		return searchKeywordRepository.findAll();
+	}
+
 	@GetMapping("/craw/naver")
 	public String crawNaver(Model model) {
 		model.addAttribute("keywords", searchKeywordRepository.findAll());
@@ -63,13 +66,12 @@ public class CrawNaverController {
 	public String crawClear() {
 		return "craw_clear";
 	}
-	
+
 	@PostMapping("/craw/naver/proc")
 	public @ResponseBody String crawNaverProc(String keyword) {
 		System.out.println(keyword);
 		List<Product> products = new CrawNaverBlog().startAllCraw(keyword);
-		SearchKeyword searchKeywordEntity = 
-				searchKeywordRepository.findByKeyword(keyword);
+		SearchKeyword searchKeywordEntity = searchKeywordRepository.findByKeyword(keyword);
 
 		for (Product product : products) {
 			product.setKeyword(searchKeywordEntity);
@@ -78,19 +80,59 @@ public class CrawNaverController {
 		productRepository.saveAll(products);
 		return "성공";
 	}
-	
+
 	@PostMapping("/craw/keyword/proc")
 	public String crawKeywordProc(String keyword) {
-		SearchKeyword entity = SearchKeyword.builder()
-				.keyword(keyword)
-				.build();
+		SearchKeyword entity = SearchKeyword.builder().keyword(keyword).build();
 		searchKeywordRepository.save(entity);
 		return "redirect:/craw/list";
 	}
 
 	@DeleteMapping("/craw/keyword/delete/{id}")
-	public ResponseEntity<?> crawKeywordDelete(@PathVariable int id){
+	public ResponseEntity<?> crawKeywordDelete(@PathVariable int id) {
 		searchKeywordRepository.deleteById(id);
 		return new ResponseEntity<String>("ok", HttpStatus.OK);
 	}
+
+	// 스프링 스케줄!!
+	public List<Product> startDayCraw(String keyword) {
+		int start = 1; //10씩 증가하면 됨.
+		List<Product> products = new ArrayList<>();
+
+		// 개수 제한
+		while (products.size() < 100) {
+			String url = "https://search.naver.com/search.naver?&date_option=0&date_to=&dup_remove=1&nso=&query=" + keyword
+					+ "&sm=tab_pge&srchby=all&st=date&where=post&start=" + start;
+
+			try {
+				Document doc = Jsoup.connect(url).get();
+				Elements els1 = doc.select(".blog .sh_blog_top .sh_blog_title");
+				Elements els2 = doc.select(".blog .sh_blog_top .txt_inline");
+				Elements els3 = doc.select(".blog .sh_blog_top .sp_thmb img");
+				for (int i = 0; i < els1.size(); i++) {
+					Product product = new Product();
+					product.setTitle(els1.get(i).attr("title"));
+					product.setBlogUrl(els1.get(i).attr("href"));
+					product.setDay(els2.get(i).text());
+					if (els3.size() <= i) {
+						product.setThumnail("사진없음");
+					} else {
+						product.setThumnail(els3.get(i).attr("src"));
+					}
+					// 오늘 날짜 것만 크롤링 데이터에서 걸러내기
+					if (product.getDay().equals(LocalDate.now().toString())) {
+						products.add(product);
+					}
+
+				}
+				System.out.println("start : " + start);
+				start = start + 10;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return products;
+	}
+
 }
